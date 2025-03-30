cdc0bedd0d16690d40c53f1e64764d637ea73797
Emin Education <143920615+bakueduaz@users.noreply.github.com>
2025-03-30 23:46:28 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
-        if (e.which == 13) {
-            let url = `https://flask-api-six-snowy.vercel.app/news`
+        if (e.which = 13) {
+            let url = ``;
+
             async function mynews() {
-                let responce = await fetch(url)
-                let data = await responce.json()
+                let responce = await fetch(url);
+                let data = await responce.json();
 
                 if (Array.isArray(data.news)) {
                     data.news.forEach(item => {
@@ -14,7 +15,7 @@ $(document).ready(function () {
                         box.innerHTML = `
                             <div class="box">
                                 <div class="imgbox">
-                                    <img src="${item.photo_url}" alt="">
+                                    <img src="${item.image}" alt="">
                                 </div>
                                 <h1>${item.title}</h1>
                                 <h4>${item.description}</h4>
@@ -31,37 +32,39 @@ $(document).ready(function () {
 
             mynews();
         }
-    })
+    });
 
     $("h2").click(async function (e) {
         e.preventDefault();
-        let url = `https://flask-api-six-snowy.vercel.app/news`
+        let url = ``;
 
-            async function mynews() {
+        async function mynews() {
             let responce = await fetch(url);
             let data = await responce.json();
-                console.log(data);
+            console.log(data);
+
             if (Array.isArray(data.news)) {
                 data.news.forEach(item => {
                     const box = document.createElement('div');
                     box.className = 'col-md-4';
                     box.innerHTML = `
-                    <div class="box">
-                        <div class="imgbox">
-                            <img src="${item.photo_url}" alt="">
-                        </div>
-                        <h1>${item.title}</h1>
-                        <h4>${item.description}</h4>
-                        <div class="infobox">
-                            <div class="lbtn">Good</div>
-                            <div class="dbtn">Bad</div>
-                        </div>
-                    </div>
-                `;
-                $(".row").append(box);
+                            <div class="box">
+                                <div class="imgbox">
+                                    <img src="${item.image}" alt="">
+                                </div>
+                                <h1>${item.title}</h1>
+                                <h4>${item.description}</h4>
+                                <div class="infobox">
+                                    <div class="lbtn">Good</div>
+                                    <div class="dbtn">Bad</div>
+                                </div>
+                            </div>
+                        `;
+                    $(".row").append(box);
                 });
             }
         }
+
         mynews();
     });
 });
